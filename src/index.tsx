0583d5abc97496bf913a3de6aa25dfff222264ba
Arcriles <arcrilest@outlook.com>
2025-10-02 19:29:11 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import ApiKeyGate from './components/ApiKeyGate';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApiKeyGate />
   </React.StrictMode>
 );
